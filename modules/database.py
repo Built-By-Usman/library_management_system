812c5ba1fc680c349067a3f37bd8168996ddfd28
usman <builtbyusman@gmail.com>
2025-12-03 22:08:38 +0500

    Add real db url

--- modules/database.py
+++ modules/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://usman:ShaniMalik321@localhost:5432/fast_api_revision"
+DATABASE_URL = "postgresql://library_management_system_o52u_user:2wPPHn1MKfrkProks9SOJwVTyfPN7HBG@dpg-d4o6qihr0fns73e47n8g-a.oregon-postgres.render.com/library_management_system_o52u"
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
